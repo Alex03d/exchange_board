ba6189d807b54cbe45d1c02eeeb3b55487f837e6
Alexander Dondokov <adondokov@gmail.com>
2023-08-18 02:26:05 +0800

    refactored templates

--- exchange_board/users/forms.py
+++ exchange_board/users/forms.py
@@ -11,7 +11,4 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = (
             'username', 'password1', 'password2',
-            'rub_bank_name', 'rub_account_or_phone', 'rub_recipient_name',
-            'usd_bank_name', 'usd_account_or_phone', 'usd_recipient_name',
-            'mnt_bank_name', 'mnt_account_or_phone', 'mnt_recipient_name'
         )
