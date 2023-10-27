ae7703c1a378a4efb1681ac66d2125f858908ebc
Alexander Dondokov <adondokov@gmail.com>
2023-10-27 23:36:05 +0800

    deleted transactions app

--- exchange_board/exchange_board/settings.py
+++ exchange_board/exchange_board/settings.py
@@ -21,7 +21,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
-    'transactions.apps.TransactionsConfig',
     'offers.apps.OffersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
