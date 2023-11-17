8fe45986c0df3c6038b6104be4cbfb0d45331491
Alexander Dondokov <adondokov@gmail.com>
2023-11-17 22:48:18 +0800

    setting amended

--- exchange_board/exchange_board/settings.py
+++ exchange_board/exchange_board/settings.py
@@ -61,6 +61,8 @@ TEMPLATES = [
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'home/ps0jc8heuqta/ashignet.club/media'
 STATIC_ROOT = '/home/ps0jc8heuqta/ashignet.club/static'
 
 WSGI_APPLICATION = 'exchange_board.wsgi.application'
@@ -103,12 +105,11 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LANGUAGE_CODE = 'en-us'
 
-MEDIA_URL = '/media/'
 
-if DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-else:
-    MEDIA_ROOT = 'home/ps0jc8heuqta/ashignet.club/media'
+# if DEBUG:
+#     MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# else:
+#     MEDIA_ROOT = 'home/ps0jc8heuqta/ashignet.club/media'
 
 # TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Ulaanbaatar'
